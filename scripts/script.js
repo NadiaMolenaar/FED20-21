516c72f11d0540849498ea88e10e8e45df4bad62
NadiaMolenaar <58073536+NadiaMolenaar@users.noreply.github.com>
2020-11-02 14:19:06 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+console.log("button-meer")
+var openMenu = document.getElementsByClassName("button-meer");
+
+openMenu.addEventListener("click", openen);
+
+function openen() {
+    openMenu.classList.toggle('menu');
+}
+
+/* var deP = document.querySelector("p");
+
+deP.addEventListener("click", ietsDoen);
+
+function ietsDoen() {
+    deP.classList.add(“anders");
+} */
