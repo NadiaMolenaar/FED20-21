81242f43d707e0219f8f6c429e940db9269fcf1c
NadiaMolenaar <58073536+NadiaMolenaar@users.noreply.github.com>
2020-11-03 12:57:29 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,27 @@
 
 
 var openMenu = document.querySelector(".button-meer");
-var nieuweArtikelen = document.querySelector(".nieuwe-artikelen");
+var nieuweArtikelen = document.querySelector(".nieuw-artikel");
+
 
 openMenu.addEventListener("click", function() {
+    // Tussen () staat het onderdeel dat aangesproken wordt in de html, namelijk de class verberg. 
+    // Daarna wordt de tekst op de button gewijzigd op het moment dat erop geklikt is.
+    if (nieuweArtikelen.classList.contains("verberg")){
+    openMenu.textContent = "Minder nieuwe recepten";
+    }
+    
+    else {
+    openMenu.textContent = "Meer nieuwe recepten";
+    }
+
+    nieuweArtikelen.classList.toggle("verberg");
+   }); 
+
+/* openMenu.addEventListener("click", function() {
     console.log("button-meer");
-    // Tussen () staat het onderdeel dat aangesproken wordt in de CSS
+    // Tussen () staat het onderdeel dat aangesproken wordt in de CSS. 
+    // Ook wordt hier dmv style.display een styling gegeven aan de nieuwe artikelen
     if (nieuweArtikelen.style.display == "none") {
         nieuweArtikelen.style.display = "block";
         openMenu.textContent = "Minder nieuwe recepten";
@@ -18,13 +34,5 @@ openMenu.addEventListener("click", function() {
         openMenu.textContent = "Meer nieuwe recepten";
         console.log("else");
     }
-});
+}); */
 
-
-/* var deP = document.querySelector("p");
-
-deP.addEventListener("click", ietsDoen);
-
-function ietsDoen() {
-    deP.classList.add(“anders");
-} */
